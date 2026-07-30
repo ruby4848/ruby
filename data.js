fdd1de99ae2d7206ce9bae1c183378cdeeabc3f0
ruby4848 <rubysatranc@gmail.com>
2026-07-30 16:02:26 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,18 @@
+const englishData = {
+    "A1": [
+        {
+            mainTitle: "BÖLÜM 1: TEMEL ATMA (Foundation)",
+            subTitle: "Unit 1: Hello, World! / Merhaba Dünya!",
+            topics: [
+                "1.1 Greetings & Farewells / Selamlaşmalar ve Vedalaşmalar",
+                "1.2 The Alphabet & Numbers (1-100) / Alfabemiz ve Sayılar",
+                "1.3 Subject Pronouns & The Verb 'To Be' (am/is/are) / Şahıs Zamirleri ve 'To Be' (Olmak) Fiili",
+                "1.4 Countries & Nationalities / Ülkeler ve Milliyetler",
+                "1.5 Personal Information (Name, Age, Job) / Kişisel Bilgiler (İsim, Yaş, Meslek)"
+            ]
+        }
+    ],
+    "A2": [],
+    "B1": [],
+    "B2": []
+};
